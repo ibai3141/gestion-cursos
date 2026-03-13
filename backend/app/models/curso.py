2c0881771ba5add7c0b1ac8362382119fd9ac1fa
ibai3141 <ibaitituanabuenano@gmail.com>
2026-03-13 13:14:45 +0100

    cambio en la clase curso

--- backend/app/models/curso.py
+++ backend/app/models/curso.py
@@ -2,13 +2,13 @@ from pydantic import BaseModel
 from typing import Optional
 
 class CursoBase(BaseModel):
-    titulo:str
-    profesor_id: int
+    titulo: str
     descripcion: Optional[str] = None
 
 class CursoCreate(CursoBase):
-    pass  # Hereda todo de CursoBase
+    pass  
 
 class CursoResponse(CursoBase):
-    id:int
+    id: int
+    profesor_id: int  
     created_at: str
